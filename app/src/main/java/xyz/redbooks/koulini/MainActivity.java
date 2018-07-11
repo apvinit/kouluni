a2605122a05aed9e170fa3a52ca75bc7ccc40c00
h4rdw1r3 <vinaugust6@gmail.com>
2018-07-11 21:15:58 +0530

    Added bottom navigation view

--- app/src/main/java/xyz/redbooks/koulini/MainActivity.java
+++ app/src/main/java/xyz/redbooks/koulini/MainActivity.java
@@ -1,13 +1,16 @@
 package xyz.redbooks.koulini;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
     }
 }
