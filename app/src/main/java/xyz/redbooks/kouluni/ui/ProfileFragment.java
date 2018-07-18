b758efdfd8c3e2302678bc3fb7232a866f3215fd
h4rdw1r3 <vinaugust6@gmail.com>
2018-07-18 14:23:57 +0530

    Added Ripple effect and clickListeners to cardView

--- app/src/main/java/xyz/redbooks/kouluni/ui/ProfileFragment.java
+++ app/src/main/java/xyz/redbooks/kouluni/ui/ProfileFragment.java
@@ -4,9 +4,11 @@ package xyz.redbooks.kouluni.ui;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import xyz.redbooks.kouluni.R;
 
@@ -15,6 +17,7 @@ import xyz.redbooks.kouluni.R;
  */
 public class ProfileFragment extends Fragment {
 
+    CardView reportCard, syllabusCard, timeTableCard;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -25,7 +28,33 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        reportCard = view.findViewById(R.id.card_reportCard);
+        reportCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Show Report Card", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        syllabusCard = view.findViewById(R.id.card_syllabus);
+        syllabusCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Show Syllabus Card", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        timeTableCard = view.findViewById(R.id.card_timeTable);
+        timeTableCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Show Timetable", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 
 }
