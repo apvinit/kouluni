581163ce8b4ce70289b6f84fa041502a9a793685
h4rdw1r3 <vinaugust6@gmail.com>
2018-07-19 14:12:46 +0530

    Gallery Implemented

---
+++ app/src/main/java/xyz/redbooks/kouluni/adapter/GalleryAdapter.java
@@ -0,0 +1,57 @@
+package xyz.redbooks.kouluni.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import xyz.redbooks.kouluni.R;
+
+public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.GalleryViewHolder> {
+
+    private int[] imagesResource = {
+            R.drawable.gallery_image1,
+            R.drawable.gallery_image2,
+            R.drawable.gallery_image3,
+            R.drawable.gallery_image4,
+            R.drawable.gallery_image5,
+            R.drawable.gallery_image6,
+            R.drawable.gallery_image7,
+            R.drawable.gallery_image8,
+            R.drawable.gallery_image9,
+            R.drawable.school1,
+            R.drawable.school2,
+            R.drawable.school3
+    };
+
+    @NonNull
+    @Override
+    public GalleryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_gallery, parent, false);
+        return new GalleryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GalleryViewHolder holder, int position) {
+        Picasso.get().load(imagesResource[position]).into(holder.galleryImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return imagesResource.length;
+    }
+
+    class GalleryViewHolder extends RecyclerView.ViewHolder {
+        ImageView galleryImage;
+
+        GalleryViewHolder(View itemView) {
+            super(itemView);
+            galleryImage = itemView.findViewById(R.id.gallery_image);
+
+        }
+    }
+}
--- app/src/main/java/xyz/redbooks/kouluni/ui/GalleryFragment.java
+++ app/src/main/java/xyz/redbooks/kouluni/ui/GalleryFragment.java
@@ -2,12 +2,18 @@ package xyz.redbooks.kouluni.ui;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import xyz.redbooks.kouluni.R;
+import xyz.redbooks.kouluni.adapter.GalleryAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,10 +27,15 @@ public class GalleryFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.grid_gallery);
+        RecyclerView.LayoutManager lm = new GridLayoutManager(getContext(), 2);
+        recyclerView.setLayoutManager(lm);
+        recyclerView.setAdapter(new GalleryAdapter());
+        return view;
     }
 
 }
