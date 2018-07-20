da026d9245362543e16367bd484019b3000c951e
h4rdw1r3 <vinaugust6@gmail.com>
2018-07-20 16:57:41 +0530

    info fragment basic ui

--- app/src/main/java/xyz/redbooks/kouluni/ui/InfoFragment.java
+++ app/src/main/java/xyz/redbooks/kouluni/ui/InfoFragment.java
@@ -2,6 +2,7 @@ package xyz.redbooks.kouluni.ui;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,10 +22,11 @@ public class InfoFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+        return view;
     }
 
 }
