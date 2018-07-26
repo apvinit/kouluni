160a001af33aba934efea3832e042102a391f39c
h4rdw1r3 <vinaugust6@gmail.com>
2018-07-26 17:31:52 +0530

    Removed tab view from about page and made about independent

--- app/src/main/java/xyz/redbooks/kouluni/ui/about/AboutFragment.java
+++ app/src/main/java/xyz/redbooks/kouluni/ui/about/AboutFragment.java
@@ -3,16 +3,12 @@ package xyz.redbooks.kouluni.ui.about;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import xyz.redbooks.kouluni.R;
-import xyz.redbooks.kouluni.adapter.AboutTabsViewPagerAdapter;
 
 /**
  * Created by h4rdw1r3
@@ -31,11 +27,6 @@ public class AboutFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_about, container, false);
-
-        TabLayout tabLayout = view.findViewById(R.id.tabs_about);
-        ViewPager pager = view.findViewById(R.id.pager_info_gallery);
-        pager.setAdapter(new AboutTabsViewPagerAdapter(getChildFragmentManager()));
-        tabLayout.setupWithViewPager(pager);
 
         return view;
     }
