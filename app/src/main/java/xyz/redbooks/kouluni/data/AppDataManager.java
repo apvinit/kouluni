aeef3b93c958640155d325e7f380b4b70e687ca1
h4rdw1r3 <vinaugust6@gmail.com>
2018-08-04 15:26:27 +0530

    Implemented DataManager

---
+++ app/src/main/java/xyz/redbooks/kouluni/data/AppDataManager.java
@@ -0,0 +1,194 @@
+package xyz.redbooks.kouluni.data;
+
+import java.util.List;
+
+import xyz.redbooks.kouluni.data.local.db.DbHelper;
+import xyz.redbooks.kouluni.data.local.prefs.PreferencesHelper;
+import xyz.redbooks.kouluni.data.model.db.Attendance;
+import xyz.redbooks.kouluni.data.model.db.Holiday;
+import xyz.redbooks.kouluni.data.model.db.Notice;
+import xyz.redbooks.kouluni.data.model.db.ParentMessage;
+
+/**
+ * Created by h4rdw1r3 at 3:16 PM on 29/7/18
+ */
+public class AppDataManager implements DataManager {
+
+    private final DbHelper appDbHelper;
+    private final PreferencesHelper appPreferenceHelper;
+
+    private AppDataManager(DbHelper dbHelper, PreferencesHelper preferencesHelper){
+        appDbHelper = dbHelper;
+        appPreferenceHelper = preferencesHelper;
+    }
+
+    @Override
+    public List<Notice> getAllNotices() {
+        return appDbHelper.getAllNotices();
+    }
+
+    @Override
+    public void insertNotice(Notice notice) {
+        appDbHelper.insertNotice(notice);
+    }
+
+    @Override
+    public List<Holiday> getAllHolidays() {
+        return appDbHelper.getAllHolidays();
+    }
+
+    @Override
+    public void insertHoliday(Holiday holiday) {
+        appDbHelper.insertHoliday(holiday);
+    }
+
+    @Override
+    public List<ParentMessage> getAllMessages() {
+        return appDbHelper.getAllMessages();
+    }
+
+    @Override
+    public void insertMessage(ParentMessage message) {
+        appDbHelper.insertMessage(message);
+    }
+
+    @Override
+    public List<Attendance> getAllAttendances() {
+        return appDbHelper.getAllAttendances();
+    }
+
+    @Override
+    public void insertAttendance(Attendance attendance) {
+        appDbHelper.insertAttendance(attendance);
+    }
+
+    @Override
+    public String getSchoolName() {
+        return appPreferenceHelper.getSchoolName();
+    }
+
+    @Override
+    public void setSetSchoolName(String schoolName) {
+        appPreferenceHelper.setSetSchoolName(schoolName);
+    }
+
+    @Override
+    public String getSchoolAddress() {
+        return appPreferenceHelper.getSchoolAddress();
+    }
+
+    @Override
+    public void setSchoolAddress(String schoolAddress) {
+        appPreferenceHelper.setSchoolAddress(schoolAddress);
+    }
+
+    @Override
+    public String getSchoolLogoUrl() {
+        return appPreferenceHelper.getSchoolLogoUrl();
+    }
+
+    @Override
+    public void setSchoolLogoUrl(String schoolLogoUrl) {
+        appPreferenceHelper.setSchoolLogoUrl(schoolLogoUrl);
+    }
+
+    @Override
+    public String getStudentName() {
+        return appPreferenceHelper.getStudentName();
+    }
+
+    @Override
+    public void setStudentName(String studentName) {
+        appPreferenceHelper.setStudentName(studentName);
+    }
+
+    @Override
+    public String getStudentFatherName() {
+        return appPreferenceHelper.getStudentFatherName();
+    }
+
+    @Override
+    public void setStudentFatherName(String fatherName) {
+        appPreferenceHelper.setStudentFatherName(fatherName);
+    }
+
+    @Override
+    public String getStudentMotherName() {
+        return appPreferenceHelper.getStudentMotherName();
+    }
+
+    @Override
+    public void setStudentMotherName(String motherName) {
+        appPreferenceHelper.setStudentMotherName(motherName);
+    }
+
+    @Override
+    public String getStudentClassStandard() {
+        return appPreferenceHelper.getStudentClassStandard();
+    }
+
+    @Override
+    public void setStudentClassStandard(String classStandard) {
+        appPreferenceHelper.setStudentClassStandard(classStandard);
+    }
+
+    @Override
+    public String getStudentDob() {
+        return appPreferenceHelper.getStudentDob();
+    }
+
+    @Override
+    public void setStudentDob(String dob) {
+        appPreferenceHelper.setStudentDob(dob);
+    }
+
+    @Override
+    public String getStudentContactNo() {
+        return appPreferenceHelper.getStudentContactNo();
+    }
+
+    @Override
+    public void setStudentContactNo(String contactNo) {
+        appPreferenceHelper.setStudentContactNo(contactNo);
+    }
+
+    @Override
+    public String getStudentAddress() {
+        return appPreferenceHelper.getStudentAddress();
+    }
+
+    @Override
+    public void setStudentAddress(String address) {
+        appPreferenceHelper.setStudentAddress(address);
+    }
+
+    @Override
+    public String getStudentProfilePicUrl() {
+        return appPreferenceHelper.getStudentProfilePicUrl();
+    }
+
+    @Override
+    public void setStudentProfilePicUrl(String profilePicUrl) {
+        appPreferenceHelper.setStudentProfilePicUrl(profilePicUrl);
+    }
+
+    @Override
+    public String getCurrentUserName() {
+        return appPreferenceHelper.getCurrentUserName();
+    }
+
+    @Override
+    public void setCurrentUserName(String userName) {
+        appPreferenceHelper.setCurrentUserName(userName);
+    }
+
+    @Override
+    public String getAccessToken() {
+        return appPreferenceHelper.getAccessToken();
+    }
+
+    @Override
+    public void setAccessToken(String accessToken) {
+        appPreferenceHelper.setAccessToken(accessToken);
+    }
+}
---
+++ app/src/main/java/xyz/redbooks/kouluni/data/DataManager.java
@@ -0,0 +1,11 @@
+package xyz.redbooks.kouluni.data;
+
+import xyz.redbooks.kouluni.data.local.db.DbHelper;
+import xyz.redbooks.kouluni.data.local.prefs.PreferencesHelper;
+
+/**
+ * Created by h4rdw1r3 at 3:15 PM on 29/7/18
+ */
+public interface DataManager extends DbHelper, PreferencesHelper {
+
+}
